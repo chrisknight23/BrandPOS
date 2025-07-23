71fb4577503c41b4db13c188f9ea219e258ae396
Chris Knight <chrisknight@BLKKRQDJ3QCGF.local>
2025-07-23 11:45:26 -0700

    Save current vite config before investigating file deletion issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sites/brandpos/', // Block cells base path
+  base: '/', // Local development base path
   server: {
     port: 3000,
     allowedHosts: [
